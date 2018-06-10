ff17f64652d72caa7a1b02d3d84814b676d437a6
BonJ <snowjaguar625@gmail.com>
2018-06-10 22:05:41 +0800

    docs: add showcase to README

--- packages/vuepress-plugin-active-header-links/index.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-
-module.exports = (options, context) => ({
-  name: 'active-header-links',
-  // This mixin will be applied to the root component of each page.
-  clientRootMixin: path.resolve(__dirname, 'mixin.js')
-})
