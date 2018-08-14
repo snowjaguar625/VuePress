b4707133609d5db8e01cb1a5ec3e5dec7474ca14
BonJ <snowjaguar625@gmail.com>
2018-08-14 10:49:41 +0800

    fix: SW-update popup only shows up on first page reload (close: #677)

--- packages/@vuepress/plugin-active-header-links/index.js
+++
@@ -1,7 +0,0 @@
-const path = require('path')
-
-module.exports = (options, context) => ({
-  name: 'active-header-links',
-  enable: context.isDev,
-  clientRootMixin: path.resolve(__dirname, 'mixin.js')
-})
--- packages/vuepress/vuepress.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-
-require('@vuepress/cli').bootstrap({
-  theme: 'default',
-  plugins: [
-    'test',
-    'i18n-ui'
-  ]
-})
