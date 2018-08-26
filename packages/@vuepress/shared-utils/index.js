bd1f8fef45b16c622691561e1954ad5aec7482c4
BonJ <snowjaguar625@gmail.com>
2018-08-26 13:24:10 -0400

    chore: improve service worker section (#766)

--- packages/@vuepress/shared-utils/index.js
+++
@@ -1,7 +0,0 @@
-exports.logger = require('./lib/logger')
-exports.env = require('./lib/env')
-exports.writeTemp = require('./lib/writeTemp')
-exports.codegen = require('./lib/codegen')
-exports.chalk = require('chalk')
-exports.fs = require('fs-extra')
-exports.globby = require('globby')
